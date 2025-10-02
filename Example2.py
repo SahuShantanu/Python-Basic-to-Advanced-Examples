1667966ab5999173d0071c6f0b679952ca0ec0ea
Sahu Shantanu <shantanusahu.in@gmail.com>
2025-10-02 10:00:27 +0530

    example2

---
+++ Example2.py
@@ -0,0 +1,18 @@
+# Write a Python program to do arithmetical operations addition and division.
+
+def add(a,b):
+    print(a+b)
+
+def sub(a,b):
+    print(a-b)
+
+def mult(a,b):
+    print(a*b)
+
+def div(a,b):
+    print(a/b)
+
+add(3,4)
+sub(5,9)
+mult(1,9)
+div(3,6)
